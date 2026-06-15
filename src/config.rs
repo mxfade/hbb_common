48927d83fbc1ac9ad4d723f803db6a0a4b02f38e
mxfade <60185528+mxfade@users.noreply.github.com>
2026-06-15 18:51:33 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -96,9 +96,10 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_DISPLAY_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref DEFAULT_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
-     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
+    pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
-        map.insert("password".to_string(), "Cencorp@2025".to_string());
+        map.insert("password".to_string(), "01AYX0Ydsy9znEZ6ftzv7GqpUrDtVZ52iTHE10JrRtg/MGdFSQ8uf8xeg1yEpybmVVWe69C15ka8rgXEc3Th5wcheIWBWwGLAKLDosSnSL76L/TqEXsV6K".to_string());
+        map.insert("salt".to_string(), "ctazgq".to_string());
         RwLock::new(map)
     };
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
